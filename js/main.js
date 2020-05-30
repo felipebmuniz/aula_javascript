0be79c26fb1c2ee42c233d3ee473fa563d4af1f2
felipebmuniz <felipemuniz@alu.ufc.br>
2020-05-29 23:05:49 -0300

    Javascript em páginas web

---
+++ js/main.js
@@ -0,0 +1,65 @@
+/*var nome = 'Felipe Barros';
+var idade = 21;
+alert(nome + " tem " + idade + " anos");
+var frase = "Hello Mundo";
+console.log(nome);
+console.log(idade + 10);
+console.log(frase.replace("Hello", "Olá"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+var lista = ["laranja", "Melancia"];
+lista.push("Uva");
+lista.pop();
+
+console.log(lista[1]);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" "));
+
+var fruta = {nome:"Maça", cor:"Vermelha"}
+
+console.log(fruta.nome);
+alert(fruta.cor);
+
+var frutas = [{nome:"Maça", cor:"Vermelha"}, {nome:"Banana", cor:"Amarela"}]
+
+console.log(frutas);
+alert(frutas[1].nome);
+
+idade = prompt("Qual sua idade");
+
+if (idade >= 18) {
+    alert("Maior de idade!!");
+} else {
+    alert("Menor de idade!!");
+}
+
+var count = 0;
+
+while(count <= 5) {
+    console.log(count);
+    count ++;
+}
+
+for (count = 0; count < 6; count++) {
+    console.log(count);
+}
+
+var d = new Date();
+alert(d.getMonth()+1);
+
+function soma(n1, n2) {
+    return n1+n2;
+}
+function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome);
+}
+
+alert(soma(1,2));
+alert(setReplace("Vai Japa","Japa","BR"));*/
+
+function botao() {
+    document.getElementById("gg").innerHTML = "Valeu pela cutucada";
+    alert("Valeu pela cutucada!!");
+}
